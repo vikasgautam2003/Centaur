fea7eae587f0a2c4f66a8d1242cd5cb7010fa05f
Vikas Gautam <vikasgautamug2003@gmail.com>
2026-01-12 06:33:05 +0530

    added Firecrawl Ai in the project --Testing Stage

--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -29,20 +29,52 @@
 
 
 
+import { firecrawl } from "@/lib/firecrawl";
 import { inngest } from "./client";
 import { google } from "@ai-sdk/google";
 import { generateText } from "ai";
 
+
+const URL_REGEX =
+  /(https?:\/\/[\w.-]+\.[a-z]{2,}(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?)/gi;
+
+
 export const demoGenerate = inngest.createFunction(
   { id: "demo-generate" },
   { event: "demo/generate" },
-  async ({ step }) => {
+  async ({ event, step }) => {
+
+    const { prompt }  = event.data as { prompt: string };
+
+    const urls = await step.run("extract-urls", async () => {
+        return prompt.match(URL_REGEX) || [];
+    })as string[];
+
+
+    const scrapedContent = await step.run("scrape-urls", async ()=>{
+        const results = await Promise.all(
+            urls.map(async(url)=>{
+                const result  = await firecrawl.scrape(
+                  url, 
+                  { formats: ["markdown"]}
+                );
+
+                return result.markdown ?? null;
+            })
+        )
+
+        return results.filter(Boolean).join("\n\n");
+    })
+
+    const finalPrompt = scrapedContent ? `Context:\n${scrapedContent}\n\nQuestion: ${prompt}`
+    : prompt;
+
     const result = await step.run(
       "generate-text",
       async () => {
         return generateText({
           model: google("gemini-2.5-flash"),
-          prompt: "Write a poem on the sea",
+          prompt: finalPrompt,
         });
       },
     
